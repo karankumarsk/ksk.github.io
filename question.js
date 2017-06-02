edb773925baae2cf1bfac3dee977dbe356900335
karankumarsk <karankumarsk@outlook.com>
2017-06-02 13:20:37 +0530

    Create question.js

---
+++ question.js
@@ -0,0 +1,37 @@
+var questions = [{
+	"question": "What is A stands for ?",
+	"option1": "Apple",
+	"option2": "Orange",
+	"option3": "Pine",
+	"option4": "Melon",
+	"answer": "1"
+}, {
+	"question": "What is B stands for ?",
+	"option1": "Ball",
+	"option2": "Cat",
+	"option3": "Computer",
+	"option4": "Machine",
+	"answer": "1"
+}, {
+	"question": "What is C stands for ?",
+	"option1": "Zebra",
+	"option2": "Calculator",
+	"option3": "Donkey",
+	"option4": "GPU",
+	"answer": "2"
+}, {
+	"question": "What is D stands for ?",
+	"option1": "Horse",
+	"option2": "Donkey",
+	"option3": "Owl",
+	"option4": "All of the above",
+	"answer": "2"
+}, {
+	"question": "What is E stands for ?",
+	"option1": "Eagle",
+	"option2": "Hippo",
+	"option3": "vulture",
+	"option4": "Anaconda",
+	"answer": "1"
+
+}];
